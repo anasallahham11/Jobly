4deeb99101d47987ec664ec6f3a01383c79117c4
Nasser117 <alnaser.h@gmail.com>
2024-07-03 10:48:47 +0300

    company_nasser

--- lib/modules/regular/company_profile/cubit/company_profile_states.dart
+++ lib/modules/regular/company_profile/cubit/company_profile_states.dart
@@ -1,2 +1,9 @@
 abstract class CompanyProfileStates{}
 class CompanyProfileInitialState extends CompanyProfileStates{}
+class CompanyLoadingState extends CompanyProfileStates{}
+class CompanySuccsssState extends CompanyProfileStates{}
+class CompanyErrorState extends CompanyProfileStates{
+  final String error;
+  CompanyErrorState(this.error);
+}
+
